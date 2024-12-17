c2bb6fadd0f456710c50a47cd1f8007b87df3de6
dario <dariobulubacich>
2024-12-17 10:36:50 -0300

    creando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/login/Login";
 import UserDashboard from "./pages/userdashboard/UserDashboard";
 import AdminDashboard from "./pages/admindashboard/AdminDashboard";
 import Ruta1Page from "./pages/ruta1/Ruta1Page"; // Nueva página
+import Ruta2Page from "./pages/ruta2/Ruta2Page"; // Nueva página
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/user-dashboard" element={<UserDashboard />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/ruta1" element={<Ruta1Page />} /> {/* Nueva ruta */}
+        <Route path="/ruta1" element={<Ruta1Page />} />
+        <Route path="/ruta2" element={<Ruta2Page />} /> {/* Nueva ruta */}
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/ruta2/Ruta2Page.jsx
@@ -0,0 +1,10 @@
+const Ruta1Page = () => {
+  return (
+    <div>
+      <h1>Esta es la página de Ruta 2</h1>
+      <p>Aquí puedes agregar el contenido específico que deseas mostrar.</p>
+    </div>
+  );
+};
+
+export default Ruta1Page;
